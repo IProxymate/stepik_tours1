3a821a3262303da1d0473b5c785069528455a10d
IProxymate <iiidonaldiii@gmail.com>
2020-08-20 16:40:00 +0300

    added info

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from tours.views import MainView, DepartureView, TourView
 
 urlpatterns = [
-    path('', MainView.as_view(), name='tours'),
-    path('departure/<str:city>/', DepartureView.as_view(), name='departure'),
-    path('tour/<int:id>', TourView.as_view(), name='tour'),
+                  path('', MainView.as_view(), name='tours'),
+                  path('departure/<str:city>/', DepartureView.as_view(), name='departure'),
+                  path('tour/<int:id>', TourView.as_view(), name='tour'),
 
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
